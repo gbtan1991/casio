820c4da3e676ef2f8a0cb365a572bbb9e1460baa
Gilbert Tan <gilbert.tan1991@gmail.com>
2023-08-13 23:01:39 +0800

    working on the stopwatch
    -working on the stopwatch
    -adjust the mode displays

--- src/js/timer.js
+++ src/js/timer.js
@@ -11,6 +11,11 @@ document.addEventListener('DOMContentLoaded', function() {
     const lapReset = document.getElementById('lapReset');
     const time24 = document.getElementById('24hrs');
     const iconWatch = document.querySelector('.icon-watch');
+    const timeFormatIcon = document.querySelector('.timeFormatIcon');
+    const displayDate = document.getElementById('date');
+    const displayDay = document.getElementById('day');
+
+    
     //SET MODE TYPE
     let mode = 0;
 
@@ -19,22 +24,30 @@ document.addEventListener('DOMContentLoaded', function() {
     const hideTime = () => { 
         clock.style.display = "none";
         stopwatch.style.display = "flex";
+
         startStop.style.display = "inline-block";
         lapReset.style.display = "inline-block";
         light.style.display = "none";  
         time24.style.display = "none";
-        
-
-        
+        timeFormatIcon.style.display = "none";
+        iconWatch.style.display = "block";
+        displayDate.style.visibility = "hidden";
+        displayDay.style.visibility = "hidden";
         
     }
     const hideStopwatch = () => {
         stopwatch.style.display = "none";
         clock.style.display = "flex";
+
         startStop.style.display = "none";
         lapReset.style.display = "none";
         light.style.display = "inline-block";
         time24.style.display = "inline-block";
+        iconWatch.style.display = "none";
+        timeFormatIcon.style.display = "block";
+        displayDate.style.visibility = "visible";
+        displayDay.style.visibility = "visible";
+        
         
     }
 
@@ -116,3 +129,64 @@ document.addEventListener('DOMContentLoaded', function() {
     const timeElement = document.getElementById(`stopwatch-hour`)
 })
 
+
+//STOPWATCH FUNCTIONALITY
+const minutesDisplay = document.getElementById('stopwatch-min');
+const secondsDisplay = document.getElementById('stopwatch-sec');
+const millisecondsDisplay = document.getElementById('stopwatch-ms');
+const startStopButton = document.getElementById('startStop');
+const resetButton = document.getElementById('lapReset');
+
+
+let timerId;
+let startTime;
+let running = false;
+let elapsedTime = 0;
+
+const formatTime = (milliseconds) => {
+    const minutes = Math.floor(milliseconds / (60 * 1000));
+    const seconds = Math.floor((milliseconds % (60 * 1000)) / 1000);
+    const millisecondsValue = milliseconds % 1000;
+    return {
+        minutes: String(minutes).padStart(2, '0'),
+        seconds: String(seconds).padStart(2, '0'),
+        milliseconds: String(millisecondsValue).padStart(2, '0')
+    };
+}
+
+const updateStopclock = () => {
+    const currentTime = new Date().getTime();
+    elapsedTime = currentTime - startTime;
+    const formattedTime = formatTime(elapsedTime);
+    minutesDisplay.textContent = formattedTime.minutes;
+    secondsDisplay.textContent = formattedTime.seconds;
+    millisecondsDisplay.textContent = formattedTime.milliseconds;
+}
+
+const toggleStartStop = () => {
+    if(!running){
+     //START THE STOPWATCH
+     startTime = new Date().getTime() - elapsedTime;
+     timerId = setInterval(updateDisplay, 10);
+    } else {
+        clearInterval(timerId);
+        timerId = null;
+        startStopButton.textContent = "Start";
+    } 
+
+    running = !running;
+ }
+
+ startStopButton.addEventListener("click", toggleStartStop);
+
+ resetButton.addEventListener("click", function(){
+    cleanInterval(timerId);
+    timerId = null;
+    elapsedTime = 0;
+    minutesDisplay.textContent = "00";
+    secondsDisplay.textContent = "00";
+    millisecondsDisplay.textContent = "00";
+    if (running) {
+        toggleStartStop();
+    }
+ });
