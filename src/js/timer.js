600bab935b601d7f9f23ce1a4fd53d60392c08f8
Gilbert Tan <57788135+gbtan1991@users.noreply.github.com>
2023-10-06 11:18:03 +0800

    Added the backlight function
    Added the backlight function

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,10 +1,10 @@
 ///CLOCK MODES
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     //MODE ELEMENT DECLARATION
-    
+
     const toggleButton = document.getElementById('changeMode');
-    const stopwatch = document.querySelector('.stopwatch-wrap'); 
+    const stopwatch = document.querySelector('.stopwatch-wrap');
     const clock = document.querySelector('.time-wrap');
     const startStop = document.getElementById('startStop');
     const light = document.getElementById('light');
@@ -14,26 +14,27 @@ document.addEventListener('DOMContentLoaded', function() {
     const timeFormatIcon = document.querySelector('.timeFormatIcon');
     const displayDate = document.getElementById('date');
     const displayDay = document.getElementById('day');
+    const timerFrame = document.getElementById('timerFrame');
 
-    
+
     //SET MODE TYPE
     let mode = 0;
 
 
     //DOM MANIPULATION FOR MODES -- THIS WILL BE CHANGE UPON DESIGNING
-    const hideTime = () => { 
+    const hideTime = () => {
         clock.style.display = "none";
         stopwatch.style.display = "flex";
 
         startStop.style.display = "inline-block";
         lapReset.style.display = "inline-block";
-        light.style.display = "none";  
+        light.style.display = "none";
         time24.style.display = "none";
         timeFormatIcon.style.display = "none";
         iconWatch.style.display = "block";
         displayDate.style.visibility = "hidden";
         displayDay.style.visibility = "hidden";
-        
+
     }
     const hideStopwatch = () => {
         stopwatch.style.display = "none";
@@ -47,19 +48,19 @@ document.addEventListener('DOMContentLoaded', function() {
         timeFormatIcon.style.display = "block";
         displayDate.style.visibility = "visible";
         displayDay.style.visibility = "visible";
-        
-        
+
+
     }
 
     //TOGGLE MODES
-    const toggleMode = () =>{
-        if (mode === 0){
+    const toggleMode = () => {
+        if (mode === 0) {
             mode = 1
             hideTime();
         } else {
             mode = 0;
             hideStopwatch();
-            
+
         }
     }
 
@@ -71,26 +72,26 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 const updateDate = () => {
-//ELEMENT DECLARATION 
-const displayDate = document.getElementById('date');
-const displayDay = document.getElementById('day');
+    //ELEMENT DECLARATION 
+    const displayDate = document.getElementById('date');
+    const displayDay = document.getElementById('day');
 
 
-//JS TO GET YEAR, MONTH, DATE
-const currentDate = new Date();
-const year = currentDate.getFullYear();
-const month = String(currentDate.getMonth() + 1).padStart(2, '0');
-const date = String(currentDate.getDate()).padStart(2, '0');
+    //JS TO GET YEAR, MONTH, DATE
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+    const date = String(currentDate.getDate()).padStart(2, '0');
 
-//INDEXING WEEKDAYS
-const week = ['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA'];
-const dayIndex = currentDate.getDay();
+    //INDEXING WEEKDAYS
+    const week = ['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA'];
+    const dayIndex = currentDate.getDay();
 
 
 
-//DISPLAYING TO DOM;
-displayDate.innerHTML = date;
-displayDay.innerHTML = week[dayIndex];
+    //DISPLAYING TO DOM;
+    displayDate.innerHTML = date;
+    displayDay.innerHTML = week[dayIndex];
 
 }
 
@@ -98,7 +99,7 @@ displayDay.innerHTML = week[dayIndex];
 
 
 //ANIMATED TIME CLOCK
-const updateClock = () => { 
+const updateClock = () => {
 
     //ELEMENT DECLARATION
     const displayHour = document.getElementById('time-hour');
@@ -190,3 +191,33 @@ function updateTimeDisplay() {
     secondsDisplay.textContent = String(secs).padStart(2, '0');
     millisecondsDisplay.textContent = String(msecs).padStart(2, '0');
 }
+
+
+
+//BACKLIGHT TAP FUNCTION
+let isHolding = false;
+
+function backLight() {
+    if(isHolding) {
+        document.body.style.background = '#0b1014';
+        timerFrame.style.background = 'linear-gradient(150deg, #0f0, #0f0, #fff)';
+        
+    } else {
+        document.body.style.background = 'radial-gradient(at 0% 0%, #393a3e 50%, #383e3e 100%)';
+        timerFrame.style.background = '#c8c197';
+    }
+     
+    }
+
+
+light.addEventListener('mousedown', () => {
+        isHolding = true;
+        backLight();
+});
+
+light.addEventListener('mouseup', () => {
+        isHolding = false;
+        backLight();
+    
+});
+
